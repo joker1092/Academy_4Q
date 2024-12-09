161d0bb578dfb76c88a5062c9a0e3dbe774d2e93
29thnight <ideneb@naver.com>
2024-12-09 09:36:03 +0900

    새로운 assertion 처리 방식 도입
    기존의 `DEFINE__ASSERT` 매크로가 제거되고, 새로운 assertion 처리 방식이 도입되었습니다. `AssertionFailureFunction` 타입 정의와 기본 assertion 실패 처리 함수 `AssertionFailureFunctionDefault`가 추가되었습니다. 이 함수는 assertion 실패 시 디버그 메시지를 출력하고, 디버거가 존재하면 디버그 브레이크를 걸며, 프로그램을 중단시킵니다. 전역 변수 `g_assertionFailureFunction`과 `g_assertionFailureFunctionContext`가 추가되어 커스텀 assertion 실패 처리 함수를 설정할 수 있게 되었습니다. `SetAssertionFailureFunction` 함수가 추가되어 사용자가 커스텀 assertion 실패 처리 함수를 설정할 수 있게 되었습니다. `AssertionFailure` 함수가 추가되어 assertion 실패 시 설정된 실패 처리 함수를 호출합니다. `CORE_ASSERT` 매크로가 변경되어 NDEBUG가 정의된 경우 assertion을 비활성화하고, 그렇지 않은 경우 새로운 assertion 처리 방식을 사용합니다.

--- Utility_Framework/Core.Assert.hpp
+++ Utility_Framework/Core.Assert.hpp
@@ -1,16 +1,60 @@
 #pragma once
 #include <cstdlib>
+#include <cstdio>
+#include <Windows.h>
 
-#define DEFINE__ASSERT(expression, ...) \
-	if(!(expression)) \
-	{ \
-		 __debugbreak(); \
-		std::abort(); \
-	} \
-	else {}
+namespace core
+{
+    // Assertion 실패 시 호출될 함수 포인터
+    using AssertionFailureFunction = void (*)(const char* expression, void* context);
 
+    // 기본 Assertion 실패 처리 함수
+    void AssertionFailureFunctionDefault(const char* expression, void* context)
+    {
+        // 기본적으로 오류 메시지 출력
+        printf("Assertion failed: %s\n", expression);
+
+        // 디버깅 환경에서 중단
+#if defined(_DEBUG)
+        if (::IsDebuggerPresent())
+        {
+            OutputDebugStringA(expression);  // 디버깅 도구에 메시지 출력
+        }
+        __debugbreak();  // 디버거 중단
+#endif
+
+        // 프로그램 종료
+        std::abort();
+    }
+
+    // 전역 변수로 Assertion 실패 함수와 컨텍스트 저장
+    static AssertionFailureFunction g_assertionFailureFunction = AssertionFailureFunctionDefault;
+    static void* g_assertionFailureFunctionContext = nullptr;
+
+    // Assertion 실패 함수 설정
+    void SetAssertionFailureFunction(AssertionFailureFunction failureFunction, void* context)
+    {
+        g_assertionFailureFunction = failureFunction;
+        g_assertionFailureFunctionContext = context;
+    }
+
+    // Assertion 실패 처리 함수 호출
+    void AssertionFailure(const char* expression)
+    {
+        if (g_assertionFailureFunction)
+        {
+            g_assertionFailureFunction(expression, g_assertionFailureFunctionContext);
+        }
+    }
+}
+
+// CORE_ASSERT 함수 정의
 #ifdef NDEBUG
-#define CORE_ASSERT(...)
+#define CORE_ASSERT(expression) ((void)0)  // NDEBUG에서 assert 비활성화
 #else
-#define CORE_ASSERT(expression, ...) DEFINE__ASSERT(expression, __VA_ARGS__);
+#define CORE_ASSERT(expression) \
+    if (!(expression))           \
+    {                            \
+        core::AssertionFailure(#expression); \
+    }
 #endif
